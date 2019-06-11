263cc77c887bf1f1f7208858424c4eba364dae36
EastEvil2018 <eastevil95@gmail.com>
2019-06-11 19:57:03 -0400

    Add WidgetController and WidgetService

--- src/main/java/com/example/wbdvsf19tdaiserverjava/controllers/WidgetController.java
+++ src/main/java/com/example/wbdvsf19tdaiserverjava/controllers/WidgetController.java
@@ -2,6 +2,7 @@ package com.example.wbdvsf19tdaiserverjava.controllers;
 
 import com.example.wbdvsf19tdaiserverjava.models.Widget;
 import com.example.wbdvsf19tdaiserverjava.models.WidgetType;
+import com.example.wbdvsf19tdaiserverjava.service.WidgetService;
 
 import org.springframework.web.bind.annotation.*;
 
@@ -12,70 +13,33 @@ import java.util.stream.Collectors;
 @RestController
 @CrossOrigin("*")
 public class WidgetController {
-    static List<Widget> widgets = new ArrayList<Widget>();
-    
-    static {
-    	Widget heading = new Widget("123");
-    	heading.setName("Widget 1");
-    	heading.setType(WidgetType.Heading);
-    	Widget para = new Widget("234");
-    	para.setName("Widget 2");
-    	para.setType(WidgetType.Paragraph);
-    	Widget img = new Widget("345");
-    	img.setName("Widget 3");
-    	img.setType(WidgetType.Image);  
-    	Widget list = new Widget("456");
-    	list.setName("Widget 4");
-    	list.setType(WidgetType.List);
-        widgets.add(heading);
-        widgets.add(para);
-        widgets.add(img);
-        widgets.add(list);
-    }
+	WidgetService widgetService = new WidgetService();
     
     @PostMapping("/api/widgets")
     public List<Widget> createWidget(@RequestBody Widget widget) {
-        widgets.add(widget);
-        return widgets;
+        return widgetService.createWidget(widget);
     }
     
     @GetMapping("/api/widgets")
     public List<Widget> findAllWidgets() {
-        return widgets;
+        return widgetService.findAllWidgets();
     }
     
     @GetMapping("/api/widgets/{widgetId}")
     public Widget findWidgetById(@PathVariable("widgetId") String wid) {
-        for(Widget w: widgets) {
-            if(w.getId().equals(wid))
-                return w;
-        }
-        return null;
+        return widgetService.findWidgetById(wid);
     }
     
     @PutMapping("/api/widgets/{widgetId}")
     public Widget updateWidget(
             @PathVariable("widgetId") String wid,
             @RequestBody Widget widget) {
-		Widget target = null;
-		for (Widget w : widgets) {
-			if (w.getId().equals(wid)) {
-				target = w;
-				break;
-			}
-		}
-		widgets.remove(target);
-		widgets.add(widget);
-        return widget;
+        return widgetService.updateWidget(wid, widget);
     }
     
     @DeleteMapping("/api/widgets/{widgetId}")
     public List<Widget> deleteWidget(@PathVariable("widgetId") String wid) {
-    	widgets = widgets
-    			.stream()
-    			.filter(widget -> !widget.getId().equals(wid))
-    			.collect(Collectors.toList());
-    	return widgets;
+    	return widgetService.deleteWidget(wid);
     }
     
 }
---
+++ src/main/java/com/example/wbdvsf19tdaiserverjava/service/WidgetService.java
@@ -0,0 +1,76 @@
+package com.example.wbdvsf19tdaiserverjava.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.example.wbdvsf19tdaiserverjava.models.Widget;
+import com.example.wbdvsf19tdaiserverjava.models.WidgetType;
+
+public class WidgetService {
+    static List<Widget> widgets = new ArrayList<Widget>();
+    
+    static {
+    	Widget heading = new Widget("123");
+    	heading.setName("Widget 1");
+    	heading.setType(WidgetType.Heading);
+    	Widget para = new Widget("234");
+    	para.setName("Widget 2");
+    	para.setType(WidgetType.Paragraph);
+    	Widget img = new Widget("345");
+    	img.setName("Widget 3");
+    	img.setType(WidgetType.Image);  
+    	Widget list = new Widget("456");
+    	list.setName("Widget 4");
+    	list.setType(WidgetType.List);
+        widgets.add(heading);
+        widgets.add(para);
+        widgets.add(img);
+        widgets.add(list);
+    }
+    
+    public List<Widget> createWidget(Widget widget) {
+        widgets.add(widget);
+        return widgets;
+    }
+    
+    public List<Widget> findAllWidgets() {
+        return widgets;
+    }
+    
+    public Widget findWidgetById(String wid) {
+        for(Widget w: widgets) {
+            if(w.getId().equals(wid))
+                return w;
+        }
+        return null;
+    }
+    
+    public Widget updateWidget(String wid,
+    						   Widget widget) {
+		Widget target = null;
+		for (Widget w : widgets) {
+			if (w.getId().equals(wid)) {
+				target = w;
+				break;
+			}
+		}
+		widgets.remove(target);
+		widgets.add(widget);
+        return widget;
+    }
+    
+    public List<Widget> deleteWidget(String wid) {
+    	widgets = widgets
+    			.stream()
+    			.filter(widget -> !widget.getId().equals(wid))
+    			.collect(Collectors.toList());
+    	return widgets;
+    }    
+}
